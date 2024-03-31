a5be430908d740c75e63964534135af59c925374
Neriya Malachi <neriamalachi@gmail.com>
2024-03-31 14:30:41 +0200

    finish pickAddons

--- app/(client)/(routes)/ui/PickAddOns/page.tsx
+++ app/(client)/(routes)/ui/PickAddOns/page.tsx
@@ -2,7 +2,37 @@
 import React, { useState } from "react";
 import { IoMdCheckmark } from "react-icons/io";
 const page = () => {
-  const [onlineService, setOnlineService] = useState(false);
+  type TG = {
+    title: string;
+    description: string;
+    price: string;
+    selected: boolean;
+    id: number;
+  };
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [typeGaming, setTypeGaming] = useState<TG[]>([
+    {
+      title: "Online service",
+      description: " Access to multiplayer games",
+      price: "1",
+      selected: false,
+      id: 1,
+    },
+    {
+      title: "larger storage",
+      description: " Extra 1TB of cloud save",
+      price: "2",
+      selected: false,
+      id: 2,
+    },
+    {
+      title: "Customizable profile",
+      description: "Custom theme on your profile",
+      price: "2",
+      selected: false,
+      id: 3,
+    },
+  ]);
   return (
     <div className="body flex justify-center bg-slate-200 h-[573px] -mt-6 ">
       <div className="card bg-white w-[90%] h-[380px] rounded-xl -mt-[19%]">
@@ -13,77 +43,56 @@ const page = () => {
           </p>
         </div>
         <div className="typesSelecte flex flex-col justify-evenly mt-4 items-center h-[65%]">
-          {/* Online service */}
-          <div
-            onClick={() => {
-              onlineService ? setOnlineService(false) : setOnlineService(true);
-            }}
-            className={`${
-              onlineService ? "border-blue-600" : "border-slate-400"
-            }flex items-center w-[93%] p-3 rounded-md border border-slate-400  hover:border-blue-600`}
-          >
-            <div className="flex justify-between  w-full items-center">
-              <div className="flex items-center ">
-                <button
-                  className={`${
-                    onlineService ? " bg-blue-800 text-white " : "bg-white text-white border"
-                  } p-1 rounded-md font-Ubuntu-Bold`}
-                >
-                  <IoMdCheckmark />
-                </button>
+          {typeGaming.map((item, index) => {
+            return (
+              <div
+                key={index}
+                onClick={() => {
+                  item.selected
+                    ? setTypeGaming((limitSelect: TG[]) =>
+                        limitSelect.map((select) =>
+                          item.id === select.id
+                            ? { ...select, selected: false }
+                            : select
+                        )
+                      )
+                    : setTypeGaming((limitSelect: TG[]) =>
+                        limitSelect.map((select) =>
+                          item.id === select.id
+                            ? { ...select, selected: true }
+                            : select
+                        )
+                      );
+                }}
+                className={`${
+                  item.selected
+                    ? "border border-blue-600"
+                    : "border border-slate-400"
+                }flex items-center w-[93%] p-3 rounded-md `}
+              >
+                <div className="flex justify-between  w-full items-center">
+                  <div className="flex items-center ">
+                    <button
+                      className={`${
+                        item.selected
+                          ? " bg-blue-800 text-white "
+                          : "bg-white text-white border"
+                      } p-1 rounded-md font-Ubuntu-Bold`}
+                    >
+                      <IoMdCheckmark />
+                    </button>
+                  </div>
+                  <div className="w-[60%]">
+                    <h1 className="font-Ubuntu-Bold text-sm">{item.title}</h1>
+                    <p className="text-xs text-slate-400">{item.description}</p>
+                  </div>
+                  <div className="">
+                    <p className="text-sm">+${item.price}/mo</p>
+                  </div>
+                </div>
               </div>
-              <div className="w-[60%]">
-                <h1 className="font-Ubuntu-Bold text-sm">Online service</h1>
-                <p className="text-xs text-slate-400">
-                  Access to multiplayer games
-                </p>
-              </div>
-              <div className="">
-                <p className="text-sm">+$1/mo</p>
-              </div>
-            </div>
-          </div>
-          {/* larger storage */}
-          <div className="flex items-center w-[93%] p-3 rounded-md border border-slate-400  hover:border-blue-600">
-            <div className="flex justify-between  w-full items-center">
-              <div className="flex items-center ">
-                <button className="text-white bg-blue-800 p-1 rounded-md font-Ubuntu-Bold ">
-                  <IoMdCheckmark />
-                </button>
-              </div>
-              <div className="  w-[60%]">
-                <h1 className="font-Ubuntu-Bold text-sm">larger storage</h1>
-                <p className="text-xs text-slate-400">
-                  Extra 1TB of cloud save
-                </p>
-              </div>
-              <div className="">
-                <p className="text-sm">+$2/mo</p>
-              </div>
-            </div>
-          </div>
-          {/*Customizable profile  */}
-          <div className="flex items-center w-[93%] p-3 rounded-md border border-slate-400  hover:border-blue-600">
-            <div></div>
-            <div className="flex justify-between  w-full items-center">
-              <div className="flex items-center ">
-                <button className="text-white bg-blue-800 p-1 rounded-md font-Ubuntu-Bold ">
-                  <IoMdCheckmark />
-                </button>
-              </div>
-              <div className="w-[60%]">
-                <h1 className="font-Ubuntu-Bold text-sm">
-                  Customizable profile
-                </h1>
-                <p className="text-xs text-slate-400">
-                  Custom theme on your profile
-                </p>
-              </div>
-              <div className="">
-                <p className="text-sm">+$2/mo</p>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
