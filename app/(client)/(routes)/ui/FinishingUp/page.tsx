202479913a147e46cb524053cfddbf367658a6d6
Neriya Malachi <neriamalachi@gmail.com>
2024-03-31 16:48:57 +0200

    end day

--- app/(client)/(routes)/ui/FinishingUp/page.tsx
+++ app/(client)/(routes)/ui/FinishingUp/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="body flex justify-center bg-slate-200 h-[573px] -mt-6 ">
+      <div className="card bg-white w-[90%] h-[420px] rounded-xl -mt-[19%]">
+        <div className="h-[30%]  flex flex-col p-6">
+          <h1 className="font-Ubuntu-Bold text-2xl">Finishing up</h1>
+          <p className="mt-4 text-slate-400">
+            Double-check evriting look OK before confirming.
+          </p>
+        </div>
+        <div className="typesSelecte flex flex-col justify-between items-center  h-[60%]">
+          <div className="flex flex-col items-center w-[85%] h-44 bg-slate-100 p-3 rounded-md   hover:border-blue-600">
+            <div className="flex justify-between  w-[100%]  ml-3">
+              <div className="flex flex-col">
+                <h1 className="font-Ubuntu-Bold">Arcade (Monthly)</h1>
+                <p className="text-slate-400">Change</p>
+              </div>
+              <p>$90/yr</p>
+            </div>
+            <div className="divide-y divide-red-900"/>
+            
+            <div className="flex w-[100%] justify-between">
+              <h1 className="font-light">Online service</h1>
+              <p>$9/mo</p>
+            </div>{" "}
+            <div className="flex w-[100%] justify-between">
+              <h1 className="font-light">Arcade</h1>
+              <p>$9/mo</p>
+            </div>
+            <div></div>
+          </div>
 
-export default page
+          <div className="TotalPrice flex justify-between w-[75%]">
+            <p>Total (per year)</p>
+            <p className="text-blue-800 font-Ubuntu-Bold">$120/yr</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/(client)/(routes)/ui/ThankYouFile/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import iconThanks from '@/assets/images/icon-thank-you.svg'
+import Image from 'next/image'
+const page = () => {
+  return (
+    <div className="body flex justify-center bg-slate-200 h-[573px] -mt-6 ">
+    <div className="card bg-white w-[90%] h-[420px] rounded-xl -mt-[19%] ">
+      <div className="h-[100%]  flex flex-col p-6 justify-evenly items-center text-center">
+        <Image src={iconThanks} alt='thanks icon' width={60} />
+        <h1 className="font-Ubuntu-Bold text-2xl -mt-7">Thank you!</h1>
+        <p className="-mt-7 w-72 text-slate-400">
+         Thanks for confirming your subscription!
+         We hope you have fun using our 
+         platform. if you have need support,
+         pleaas feel free to email us at  
+         support@gmail.com.
+        </p>
+      </div>
+     
+    </div>
+  </div>
+  )
+}
+
+export default page
